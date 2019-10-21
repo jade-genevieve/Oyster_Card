f773abc1b51a63a651539807d6db781aee04d1be
slongboro <susanlongley@gmail.com>
2019-10-21 14:20:47 +0100

    add second test

---
+++ lib/oyster_card.rb
@@ -0,0 +1,19 @@
+class OysterCard
+
+  attr_reader :balance
+
+  def initialize
+    #@add_funds
+    @balance = 0
+  end
+
+  #def balance
+  #  @balance = balance
+  #end
+
+  def top_up(amount)
+    #@add_funds = add_funds
+    
+  end
+
+end
--- spec/Oyster_Card_Spec.rb
+++ spec/Oyster_Card_Spec.rb
@@ -1,8 +1,16 @@
+require 'oyster_card'
+
 describe OysterCard do
   subject {OysterCard.new}
 
-it "It should allow the customer to add funds to the card" do
-  expect(subject.add_funds(10)).to eq 10
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  it "It should allow the customer to add funds to the card" do
+    expect { subject.top_up 1}.to change{subject.balance }.by 1
+  end
+
+it "has a balance of zero" do
+  expect(subject.balance).to eq(0)
 end
 
 end
